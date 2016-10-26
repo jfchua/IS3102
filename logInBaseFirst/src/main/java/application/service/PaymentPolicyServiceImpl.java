00205afe5c590f1629e01673d977ee76cdc20e6f
mingshazz <tkgs.zhao.mingsha@gmail.com>
2016-10-27 01:34:01 +0800

    calculate turnover

--- logInBaseFirst/src/main/java/application/service/PaymentPolicyServiceImpl.java
+++ logInBaseFirst/src/main/java/application/service/PaymentPolicyServiceImpl.java
@@ -1,8 +1,12 @@
 package application.service;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
+import org.apache.commons.lang.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +14,11 @@ import org.springframework.stereotype.Service;
 
 import application.entity.ClientOrganisation;
 import application.entity.PaymentPolicy;
+import application.entity.Role;
+import application.entity.Event;
+import application.entity.PaymentPlan;
 import application.entity.SpecialRate;
+import application.entity.User;
 import application.repository.ClientOrganisationRepository;
 import application.repository.PaymentPolicyRepository;
 @Service
@@ -88,5 +96,43 @@ public class PaymentPolicyServiceImpl implements PaymentPolicyService {
 	public Optional<PaymentPolicy> getPaymentPolicy(long id) {
 		LOGGER.debug("Getting special rate={}", id);
 		return Optional.ofNullable(paymentPolicyRepository.findOne(id));
+	}
+
+	@Override
+	public Double calculateTurnover(ClientOrganisation client) {
+		Calendar cal = Calendar.getInstance();
+		int month = cal.get(Calendar.MONTH);
+		Double totalDe = 0.00;
+		Double begin = 0.00;
+		Double end = 0.00;
+		
+		Calendar cal1 = Calendar.getInstance();
+		cal1.set(Calendar.DAY_OF_MONTH, cal1.getActualMinimum(Calendar.DAY_OF_MONTH));
+		Date d1 = cal1.getTime();
+		cal1.set(Calendar.DAY_OF_MONTH, cal1.getActualMaximum(Calendar.DAY_OF_MONTH));
+		Date d2 = cal1.getTime();
+		
+		Set<User> users = client.getUsers();
+		for(User u: users){
+			Set<Role> roles = u.getRoles();
+			for(Role r : roles){
+				if(r.getName().equals("ROLE_EXTEVE")){
+					Set<Event> events = u.getEvents();
+					for(Event e : events){
+						PaymentPlan pay = e.getPaymentPlan();
+						cal.setTime(pay.getCreated());
+						int month1 = cal.get(Calendar.MONTH);
+						if(month== month1){
+						totalDe += pay.getDeposit();	
+						}
+						if(DateUtils.isSameDay(d1, pay.getCreated()))
+							begin += pay.getDeposit();
+						else if(DateUtils.isSameDay(d2, pay.getCreated()))
+							end += pay.getDeposit();
+					}
+				}
+			}
+		}
+		return 2*totalDe/(begin+end);
 	}
 }
