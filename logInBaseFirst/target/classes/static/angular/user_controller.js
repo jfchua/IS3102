c5d62e062557efbe42571897ef357503b520bb15
Hailing <tkgs.zhang.hailing@gmail.com>
2016-10-27 18:08:12 +0800

    move code out from user_controller

--- logInBaseFirst/target/classes/static/angular/user_controller.js
+++ logInBaseFirst/target/classes/static/angular/user_controller.js
@@ -243,48 +243,8 @@ app.controller('UserController', ['$scope', 'UserService','$stateParams', '$rout
 			$scope.ctrler.pass1 = '';*/
 	}
 
-	var viewUserInfo = function(){
-		$scope.userInfo = [];	
-		$http.get("//localhost:8443/user/viewCurrentUser").then(function(response){
-			console.log("test Hailing");
-			console.log(response);
-			console.log(response.data);
-			$scope.userInfo = angular.fromJson(response.data);
-			console.log("DISPLAY current user");
-			if(response.data.theme){
-						$('<link>')
-							  .appendTo('head')
-				 			  .attr({type : 'text/css', rel : 'stylesheet'})
-							  .attr('href', 'css/styles/app-'+response.data.theme+'.css');
-							console.log("THEME IS "+response.data.theme);
-						}
-		},function(response){
-			alert("did not view user info");
-		}	
-		)	
-	}
-	
-	viewUserInfo();
-	$scope.logo = "";
-	 	var getLogo = function getLogo(){
-	
-	 		$http({
-	 			method: 'GET',
-	 			url: 'https://localhost:8443/getCompanyLogo'
-			}).success(function (result) {
-	 			if(result)
-	 			$scope.logo = result;
-				else
-	 			$scope.logo="img/ifms.png";
-	 			console.log(result);
-	 			console.log("LOGO: " + JSON.stringify(result));
-	 		}).error(function(result){
-	 			//do something
-	 			
-	 			console.log("LOGOERROR: " + JSON.stringify(result));
-	 		});
-	 	}
-	 	getLogo();
+
+
 	 	
 
 
