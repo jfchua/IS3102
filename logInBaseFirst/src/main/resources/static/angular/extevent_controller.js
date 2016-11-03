7a2ad542eeaf01e2c0d590c334cc11ba3f30339f
Hailing <tkgs.zhang.hailing@gmail.com>
2016-11-03 11:39:38 +0800

    update event picture and calendar

--- logInBaseFirst/src/main/resources/static/angular/extevent_controller.js
+++ logInBaseFirst/src/main/resources/static/angular/extevent_controller.js
@@ -470,6 +470,7 @@ app.controller('addEController', ['$scope', '$http','$state','$routeParams','sha
 
 		$scope.currentlySelectedUnit;
 		$scope.selectUnit = function(){
+			$scope.haha.length=0;
 			console.log("currently selected unit bookings");
 			console.log($scope.currentlySelectedUnit.bookings);
 
@@ -940,7 +941,7 @@ app.controller('updateEController', ['$scope', '$http','$state','$routeParams','
 		console.log("GETTING THE ALL UNITS INFO")
 		var getUnits = $http({
 			method  : 'POST',
-			url     : 'https://localhost:8443/property/viewUnits/',
+			url     : 'https://localhost:8443/property/viewUnitsWithBookings/',
 			data    : dataObj,
 		});
 		console.log("REACHED HERE FOR SUBMIT LEVEL " + JSON.stringify(dataObj));
@@ -956,7 +957,11 @@ app.controller('updateEController', ['$scope', '$http','$state','$routeParams','
 
 		$scope.currentlySelectedUnit;
 		$scope.selectUnit = function(){
-
+			$scope.haha.length=0;
+			getEvents($scope.currentlySelectedUnit.bookings);
+			console.log("currently selected unit schedules");
+			console.log($scope.currentlySelectedUnit.schedule);
+			getMaints($scope.currentlySelectedUnit.schedule);//put here or after for loop
 			var duplicate = false;
 			var index = 0;
 			angular.forEach($scope.selectedBookingsUnits, function() {
@@ -1210,6 +1215,73 @@ app.controller('updateEController', ['$scope', '$http','$state','$routeParams','
 			};
 		});
 	};	
+	
+	$scope.uiConfig = {
+			calendar:{
+				width: 300,
+				editable:false,
+				header:{
+					left: 'month agendaWeek agendaDay',
+					center: 'title',
+					right: 'today prev,next'
+				},
+				eventClick: $scope.alertEventOnClick,
+				eventDrop: $scope.alertOnDrop,
+				eventResize: $scope.alertOnResize
+			}
+	};
+    
+    
+    
+    $scope.haha=[];
+	//RETRIEVE EVENTS
+	//$scope.eventsFormated=[];
+	var getEvents = function(bookings){
+		//need to changed to same as workspace calendar view all events with status success approved,processing	
+		var index=0;
+		angular.forEach(bookings, function() {
+
+			var booking=[{start: bookings[index].event_start_date_time,
+				end: bookings[index].event_end_date_time,	         
+				title:'Booked',
+				allDay: false,
+				color: 'IndianRed',
+				overlap:false
+			}];
+
+			$scope.haha.push(booking);
+			index = index + 1;
+		});	
+
+
+
+
+	}
+	// getEvents(); 
+
+
+	//RETRIEVE MAINTENANCES
+	//$scope.eventsFormated=[];
+	var getMaints = function(schedules){
+		var index=0;
+		angular.forEach(schedules, function() {
+
+			var maint=[{start: schedules[index].start_time,
+				end: schedules[index].end_time,	         
+				title:"Maintenance",
+				allDay: false,
+				color: 'SteelBlue'
+			}];
+
+			$scope.haha.push(maint);
+			index = index + 1;
+		});
+		//var buildings ={name: $scope.name, address: $scope.address};
+		console.log( $scope.haha);
+
+
+	}
+
 }]);
 
 
