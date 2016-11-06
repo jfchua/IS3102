75143c00dbd816106786310eecd7aea239f29e04
Jaydentkh <jaydentkh@gmail.com>
2016-11-07 00:03:02 +0800

    checking data.sql

--- logInBaseFirst/src/main/resources/data.sql
+++ logInBaseFirst/src/main/resources/data.sql
@@ -24,14 +24,14 @@ INSERT INTO payment_policy(deposit_rate, interim_period, due_days, subsequent_nu
 VALUES(0.4, 2, 2, 2);
 
 --ALTER TABLE client_organisation ENGINE=Innodb;
-insert into client_organisation (organisation_name, address, end_date, fee, postal, phone, start_date, payment_policy_id,themecolour,logo_file_path) 
-values ("Expo", "1 Expo Dr", "2017-06-30 22:00:00", "45000.00", "486150", "64032160", "2016-07-01 22:00:00", 1,"midnight-blue","expoLogo.png");
-insert into client_organisation (organisation_name, address, end_date, fee, postal, phone, start_date, themecolour,logo_file_path) 
-values ("Suntec", "3 Temasek Boulevard", "2017-06-30 22:00:00", "25000.00", "038983", "68221537", "2016-07-01 22:00:00", "green","suntecLogo.png");
+insert into client_organisation (organisation_name, address, postal, phone, payment_policy_id,themecolour,logo_file_path) 
+values ("Expo", "1 Expo Dr", "486150", "64032160", 1,"midnight-blue","expoLogo.png");
+insert into client_organisation (organisation_name, address, postal, phone,themecolour,logo_file_path) 
+values ("Suntec", "3 Temasek Boulevard","038983", "68221537","green","suntecLogo.png");
 
 --Algattas is client_organisation 3
---insert into client_organisation (organisation_name, address, postal, phone,themecolour,logo_file_path) 
---values ("Algattas", "73 Ubi Road 1","408733", "31580456","red","algattasLogo.png");
+insert into client_organisation (organisation_name, address, postal, phone,themecolour,logo_file_path) 
+values ("Algattas", "73 Ubi Road 1","408733", "31580456","red","algattasLogo.png");
 INSERT INTO user (email, password_hash,client_organisation_id, name ,security, security_question)
 VALUES ('superadmin@localhost', '$2a$04$skH54MFgrVwCYx1lCbDgz.icEeks2GAIfEMi5y1ENtP9klVzj39w6',3 ,'Kenneth LIM' , '$2a$04$skH54MFgrVwCYx1lCbDgz.icEeks2GAIfEMi5y1ENtP9klVzj39w6','favourite number' );
 --Algattas superadmin is user 1
@@ -57,6 +57,7 @@ INSERT INTO user (email, password_hash,client_organisation_id, name ,security, s
 VALUES ('suntecadmin@localhost', '$2a$04$skH54MFgrVwCYx1lCbDgz.icEeks2GAIfEMi5y1ENtP9klVzj39w6',2, 'Suntec Admin', '$2a$04$skH54MFgrVwCYx1lCbDgz.icEeks2GAIfEMi5y1ENtP9klVzj39w6', 'favourite number');
 INSERT INTO user (email, password_hash,client_organisation_id, name ,security, security_question)
 VALUES ('suntecall@localhost', '$2a$04$skH54MFgrVwCYx1lCbDgz.icEeks2GAIfEMi5y1ENtP9klVzj39w6',2, 'Suntec All roles', '$2a$04$skH54MFgrVwCYx1lCbDgz.icEeks2GAIfEMi5y1ENtP9klVzj39w6', 'favourite number');
+
 --User 12 to 13
 INSERT INTO user (email, password_hash,client_organisation_id, name ,security, security_question)
 VALUES ('tkgs.zhao.mingsha@gmail.com', '$2a$04$skH54MFgrVwCYx1lCbDgz.icEeks2GAIfEMi5y1ENtP9klVzj39w6',1,'Mingsha ZHAO', '$2a$04$skH54MFgrVwCYx1lCbDgz.icEeks2GAIfEMi5y1ENtP9klVzj39w6', 'favourite number');
