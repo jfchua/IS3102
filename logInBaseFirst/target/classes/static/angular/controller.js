7345906384ee9c89ffba35351191ab0f19649b82
kenneth <a0124975@u.nus.edu>
2016-11-08 12:44:52 +0800

    ppp

--- logInBaseFirst/target/classes/static/angular/controller.js
+++ logInBaseFirst/target/classes/static/angular/controller.js
@@ -9,20 +9,33 @@ app.controller('passController', function($scope) {
     $scope.headingTitle = "Pass List";
 });
 
-app.controller('dashboardStateController', ['$state', '$stateParams', '$sessionStorage', function($state, $stateParams, $sessionStorage) {
+app.controller('dashboardStateController', ['$state', '$stateParams', '$sessionStorage', '$location', '$window','Auth', function($state, $stateParams, $sessionStorage, $location, $window,Auth) {
     //$scope.headingTitle = "Pass List";
 		console.log($stateParams);
 		console.log($stateParams.param);
-
-
-	    if ($stateParams.param != sessionStorage.getItem('clientOrg')){
+		if (Auth.isAuthenticated()) {
+		//this handles changing client organisation forcefully
+		    if ($stateParams.param != sessionStorage.getItem('clientOrg')){
+				event.preventDefault();
+				console.log(sessionStorage.getItem('clientOrg'));
+				console.log($stateParams.param);
+		    	$stateParams.param = sessionStorage.getItem('clientOrg');
+		    	//$state.reload();
+		    	console.log($stateParams.param);
+		    	alert('Error attempting to access beyond your organisation');
+				//$location.path('/dashboard/'+ ($stateParams.param)+'/workspace');
+				console.log("session at error attempting... in controller");
+				console.log($location.path());
+				$state.reload();	
+		    		}
+				}
+		else{
 			event.preventDefault();
-			console.log(sessionStorage.getItem('clientOrg'));
-			console.log($stateParams.param);
-	    	$stateParams.param = sessionStorage.getItem('clientOrg');
-	    	//$state.reload();
-	    	
-	    }
-	    $state.go('dashboard.workspace');
-	  }
+	    	alert('You are not logged in!');
+	    	$state.go('/login');
+			
+		}
+			
+			
+	}
 ]);
