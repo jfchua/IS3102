e3e24306e39b12e4ad9962a0ddca89bd7476a19a
Jaydentkh <jaydentkh@gmail.com>
2016-11-10 02:30:36 +0800

    Add files via upload

--- logInBaseFirst/src/main/resources/static/angular/app.js
+++ logInBaseFirst/src/main/resources/static/angular/app.js
@@ -1696,6 +1696,47 @@ app.factory('datfactory', function ($http, $q){
 	}
 	return this;
 });
+app.directive('topnav',function(){
+	return {
+		templateUrl:'views/topnav.html?v='+window.app_version,
+		restrict: 'E',
+		replace: true,
+		controller: function($scope){
+
+			$scope.showMenu = function(){
+
+				$('.dashboard-page').toggleClass('push-right');
+
+			}
+			$scope.changeTheme = function(setTheme){
+
+				$('<link>')
+				.appendTo('head')
+				.attr({type : 'text/css', rel : 'stylesheet'})
+				.attr('href', 'styles/app-'+setTheme+'.css?v='+window.app_version);
+
+				// $.get('/api/change-theme?setTheme='+setTheme);
+
+			}
+			$scope.rightToLeft = function(){
+				// alert('message');
+				$('body').toggleClass('rtl');
+
+				// var t = $('body').hasClass('rtl');
+				// console.log(t);
+
+				if($('body').hasClass('rtl')) {
+					$('.stat').removeClass('hvr-wobble-horizontal');
+				}
+
+
+			}
+
+
+
+		}
+	}
+});
 
 
 
