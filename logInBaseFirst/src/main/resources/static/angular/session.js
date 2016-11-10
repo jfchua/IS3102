17471b0fece59ca9275f277f964f4dee34deaf32
Jaydentkh <jaydentkh@gmail.com>
2016-11-10 12:24:01 +0800

    Add files via upload

--- logInBaseFirst/src/main/resources/static/angular/session.js
+++ logInBaseFirst/src/main/resources/static/angular/session.js
@@ -1,12 +1,12 @@
 app.run(['$rootScope', 'AUTH_EVENTS', 'Auth' ,'$location','$window', '$sessionStorage','$state','$stateParams','$http', function ($rootScope, AUTH_EVENTS, Auth, $location, $window , $sessionStorage, $state, $stateParams, $http) {
 
 			$rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
-			
+				
 				var authorizedRoles = toState.data.authorizedRoles;
 				console.log("Next authorized roles are " + authorizedRoles);
-
 				if (fromState.name !== '/login' && toState.name !== '/login' 
-					&& toState.name !== 'IFMS' && fromState.name !== 'IFMS' && fromState.name !== '/resetPassword' && toState.name !== '/resetPassword' ) {
+					&& toState.name !== 'IFMS' && fromState.name !== 'IFMS'
+						&& fromState.name !== '/'&& fromState.name !== '404') {
 					
 					if (sessionStorage.getItem('user') && $stateParams.param === undefined) {
 						console.log('statechange sessionStorage get item is not null');					
@@ -16,7 +16,7 @@ app.run(['$rootScope', 'AUTH_EVENTS', 'Auth' ,'$location','$window', '$sessionSt
 				 	if ($stateParams.param != sessionStorage.getItem('clientOrg')){
 						event.preventDefault();
 						$stateParams.param = sessionStorage.getItem('clientOrg');
-						$state.go(toState.name, {param : $stateParams.param});					
+						$state.go(toState.name, {param : $stateParams.param}, {reload :true});					
 					}
 				 	if (!Auth.isAuthorized(authorizedRoles)) {
 						console.log("role is authorizing")
@@ -26,7 +26,7 @@ app.run(['$rootScope', 'AUTH_EVENTS', 'Auth' ,'$location','$window', '$sessionSt
 							console.log("notauthorised");
 							$window.location.reload();
 							alert('You are not authorised to view this page!');
-							$state.go('IFMS.workspace', {param :sessionStorage.getItem('clientOrg')});
+							$state.go('IFMS.workspace', {param :sessionStorage.getItem('clientOrg')}, {reload :true});
 
 						} else {
 							// user is not logged in
@@ -38,41 +38,7 @@ app.run(['$rootScope', 'AUTH_EVENTS', 'Auth' ,'$location','$window', '$sessionSt
 				 	}
 				}
 			}
-			//	})
-			
-				/*	if ($location.path() != '/login'){
-						if ( $location.path() != '/#/login') {
-							if ($location.path() != '/reset') {
-								if (  $location.path().indexOf('/resetPassword/') == -1) {
-									//if (Auth.getUser() != null) {
-									if (!Auth.isAuthorized(authorizedRoles)) {
-										console.log("role is authorizing")
-										event.preventDefault();
-										if (Auth.isAuthenticated()) {
-											// user is not allowed
-											console.log("notauthorised");
-											$window.location.reload();
-											alert('You are not authorised to view this page!');
-											//  $window.location.href = '/#/401';
-											//   $rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
-										} else {
-											// user is not logged in
-											console.log("notauthenticated");
-											// $location.path('/login');
-											alert('You are not logged in!');
-											$window.location.href = '/#/login';
-										}
-										//  $rootScope.$broadcast(AUTH_EVENTS.notAuthenticated);
-									}
-									
-										
-								}
-							}}}*/
-			
-				
-			
-			
-				
+
 			
 			);
 
